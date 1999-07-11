7c56078a3cd1320db84f7043238abb0aa0928e25
pavel <pavel>
1999-07-11 18:18:58 +0000

    Alarm support for GUI.

--- xgnokii.c
+++ xgnokii.c
@@ -26,34 +26,34 @@
 #include "pixmaps/logo.xpm"
 #include "pixmaps/background.xpm"
 #include "pixmaps/sms.xpm"
+#include "pixmaps/alarm.xpm"
 
 GtkWidget *GUI_SplashWindow;
 GtkWidget *GUI_MainWindow;
 
 /* Pixmap used for drawing all the stuff. */
-
 static GdkPixmap *Pixmap = NULL;
 
 /* Pixmap used for background. */
-
 static GdkPixmap *BackgroundPixmap = NULL;
 
 /* Pixmap used for SMS picture. */
-
 static GdkPixmap *SMSPixmap = NULL;
 
-char *Model;	/* Model from .gnokiirc file. */
-char *Port;		/* Serial port from .gnokiirc file */
+/* Pixmap used for alarm picture. */
+static GdkPixmap *AlarmPixmap = NULL;
 
-	/* Local variables */
-char *DefaultModel = MODEL;	/* From Makefile */
+char *Model; /* Model from .gnokiirc file. */
+char *Port;  /* Serial port from .gnokiirc file */
+
+/* Local variables */
+char *DefaultModel = MODEL; /* From Makefile */
 char *DefaultPort = PORT;
 
-/* Variables which contains current signal and battery. */
-
+/* Variables which contain current signal and battery. */
 static float rflevel=-1, batterylevel=-1;
 static GSM_PowerSource powersource=-1;
-
+static GSM_DateTime Alarm;
 static GSM_SMSStatus SMSStatus = {0, 0}; 
 
 GSM_Error fbusinit(bool enable_monitoring)
@@ -77,7 +77,6 @@ GSM_Error fbusinit(bool enable_monitoring)
 
   return *GSM_LinkOK;
 }
-
 
 void GUI_DrawBackground(GtkWidget *data) {
 
@@ -131,6 +130,7 @@ void GUI_DrawBattery(GtkWidget *data, int batterylevel) {
 }
 
 void GUI_DrawSMS(GtkWidget *data) {
+
     gdk_draw_pixmap(Pixmap,
 		    GTK_WIDGET(data)->style->fg_gc[GTK_STATE_NORMAL],
 		    SMSPixmap,
@@ -139,7 +139,18 @@ void GUI_DrawSMS(GtkWidget *data) {
 		    26, 7);
 }
 
+void GUI_DrawAlarm(GtkWidget *data) {
+
+    gdk_draw_pixmap(Pixmap,
+		    GTK_WIDGET(data)->style->fg_gc[GTK_STATE_NORMAL],
+		    AlarmPixmap,
+		    0, 0,
+		    163, 11,
+		    9, 9);
+}
+
 void GUI_DrawSMSReceived(GtkWidget *data) {
+
   static GdkFont *Font;
 
   Font = gdk_font_load ("-misc-fixed-medium-r-*-*-*-100-*-*-*-*-*-*");
@@ -179,6 +190,9 @@ gint GUI_Update(gpointer data) {
 
   GUI_DrawBattery(data, batterylevel);
 
+  if (GSM->GetAlarm(0, &Alarm) == GE_NONE && Alarm.AlarmEnabled!=0)
+    GUI_DrawAlarm(data);
+
   if (GSM->GetSMSStatus(&SMSStatus) == GE_NONE && SMSStatus.UnRead>0) {
     GUI_DrawSMS(data);
 
@@ -197,7 +211,6 @@ gint GUI_Update(gpointer data) {
   return TRUE;
 }
 
-
 /* Redraw the screen from the backing pixmap */
 
 static gint GUI_ExposeEvent (GtkWidget *widget, GdkEventExpose *event)
@@ -212,6 +225,24 @@ static gint GUI_ExposeEvent (GtkWidget *widget, GdkEventExpose *event)
   return FALSE;
 }
 
+static gint GUI_ButtonPressEvent (GtkWidget *widget, GdkEventButton *event)
+{
+
+  /* Left button */
+  if (event->button == 1) {
+
+    /* Close */
+    if (event->x >= 206 && event->x <= 221 &&
+	event->y >=  42 && event->y <= 55)
+      exit(0);
+
+  }
+
+  //  printf ("event->x: %f\n", event->x);
+  //  printf ("event->y: %f\n", event->y);
+
+  return TRUE;
+}
 
 void GUI_Refresh() {
   while (gtk_events_pending())
@@ -225,13 +256,15 @@ void GUI_TopLevelWindow() {
   GtkStyle *style;
   GdkGC *gc;
 
-  GUI_MainWindow = gtk_window_new(GTK_WINDOW_POPUP);
-  // GUI_MainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  // GUI_MainWindow = gtk_window_new(GTK_WINDOW_POPUP);
+  GUI_MainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_widget_realize (GUI_MainWindow);
 
   BackgroundPixmap = gdk_pixmap_create_from_xpm_d(GUI_MainWindow->window,&mask, &GUI_MainWindow->style->white, (gchar **) XPM_background);
 
   SMSPixmap = gdk_pixmap_create_from_xpm_d(GUI_MainWindow->window,&mask, &GUI_MainWindow->style->white, (gchar **) XPM_sms);
+
+  AlarmPixmap = gdk_pixmap_create_from_xpm_d(GUI_MainWindow->window,&mask, &GUI_MainWindow->style->white, (gchar **) XPM_alarm);
 
   Pixmap = gdk_pixmap_create_from_xpm_d(GUI_MainWindow->window,&mask, &GUI_MainWindow->style->white, (gchar **) XPM_background);
 
@@ -242,6 +275,18 @@ void GUI_TopLevelWindow() {
   /* Create the drawing area */
 
   drawing_area = gtk_drawing_area_new ();
+
+  /* Signals used to handle backing pixmap */
+
+  gtk_signal_connect (GTK_OBJECT (drawing_area), "expose_event",
+		      (GtkSignalFunc) GUI_ExposeEvent, NULL);
+
+  gtk_signal_connect (GTK_OBJECT (drawing_area), "button_press_event",
+		      (GtkSignalFunc) GUI_ButtonPressEvent, NULL);
+
+  gtk_widget_set_events (drawing_area, GDK_EXPOSURE_MASK
+			 | GDK_BUTTON_PRESS_MASK);
+
   gtk_drawing_area_size (GTK_DRAWING_AREA (drawing_area), 261, 96);
   gtk_container_add(GTK_CONTAINER(GUI_MainWindow), drawing_area);
 
@@ -251,11 +296,6 @@ void GUI_TopLevelWindow() {
 		  0, 0,
 		  0, 0,
 		  261, 96);
-
-  /* Signals used to handle backing pixmap */
-
-  gtk_signal_connect (GTK_OBJECT (drawing_area), "expose_event",
-		      (GtkSignalFunc) GUI_ExposeEvent, NULL);
 
   gtk_widget_shape_combine_mask(GUI_MainWindow, mask, 0, 0);
     
@@ -312,31 +352,29 @@ int GUI_RemoveSplash(GtkWidget *Win) {
   return FALSE;
 }
 
-void	GUI_ReadConfig(void)
+void GUI_ReadConfig(void)
 {
-    struct CFG_Header *cfg_info;
-	char				*homedir;
-	char				rcfile[200];
 
-	homedir = getenv("HOME");
+  struct CFG_Header *cfg_info;
+  char *homedir;
+  char rcfile[200];
 
-	strncpy(rcfile, homedir, 200);
-	strncat(rcfile, "/.gnokiirc", 200);
+  homedir = getenv("HOME");
 
-    if ((cfg_info = CFG_ReadFile(rcfile)) == NULL) {
-		fprintf(stderr, "error opening %s, using default config\n", 
-		  rcfile);
-    }
+  strncpy(rcfile, homedir, 200);
+  strncat(rcfile, "/.gnokiirc", 200);
 
-    Model = CFG_Get(cfg_info, "global", "model");
-    if (Model == NULL) {
-		Model = DefaultModel;
-    }
+  if ((cfg_info = CFG_ReadFile(rcfile)) == NULL)
+    fprintf(stderr, "error opening %s, using default config\n", 
+	    rcfile);
 
-    Port = CFG_Get(cfg_info, "global", "port");
-    if (Port == NULL) {
-		Port = DefaultPort;
-    }
+  Model = CFG_Get(cfg_info, "global", "model");
+  if (Model == NULL)
+    Model = DefaultModel;
+
+  Port = CFG_Get(cfg_info, "global", "port");
+  if (Port == NULL)
+    Port = DefaultPort;
 
 }
 
