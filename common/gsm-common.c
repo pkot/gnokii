4eaa51d3de46ec763fdb4f30f028b41391c7a26e
pkot <pkot>
2001-01-12 14:28:39 +0000

    Forgot to add this file. ;-)

---
+++ common/gsm-common.c
@@ -0,0 +1,25 @@
+/*
+
+  $Id$
+
+  G N O K I I
+
+  A Linux/Unix toolset and driver for Nokia mobile phones.
+
+  Copyright (C) 1999, 2000 Hugh Blemings & Pavel Janík ml.
+
+  Released under the terms of the GNU GPL, see file COPYING for more details.
+
+  $Log$
+  Revision 1.1  2001-01-12 14:28:39  pkot
+  Forgot to add this file. ;-)
+
+
+*/
+
+#include <gsm-common.h>
+
+GSM_Error Unimplemented(void)
+{
+	return GE_NOTIMPLEMENTED;
+}
