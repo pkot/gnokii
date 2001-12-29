cb381cc2a1696adfb817124b3ccd9fa1ff684ea3
pkot <pkot>
2001-12-29 22:17:28 +0000

    Do not send SMS twice in 7110/6210

--- common/phones/nk7110.c
+++ common/phones/nk7110.c
@@ -842,10 +842,7 @@ static GSM_Error P7110_SendSMS(GSM_Data *data, GSM_Statemachine *state)
 	}
 	dprintf("\n");
 	if (SM_SendMessage(state, length, 0x02, req) != GE_NONE) return GE_NOTREADY;
-	while (1) {
-		e = SM_Block(state, data, 0x02);
-		if (e == GE_SMSSENDOK || e == GE_SMSSENDFAILED) break;
-	}
+	e = SM_BlockNoRetry(state, data, 0x02);
 	return e;
 }
 
