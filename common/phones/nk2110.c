9e4e052ced08ac3f850ab49182f83ca0504f5881
machek <machek>
2002-01-27 23:16:46 +0000

    Do not burn 100% cpu on polling sms-es.

--- common/phones/nk2110.c
+++ common/phones/nk2110.c
@@ -1233,7 +1233,14 @@ GSM_Error P2110_Functions(GSM_Operation op, GSM_Data *data, GSM_Statemachine *st
 			err = SMS_Reserve(state);
 		}
 		break;		
-	case GOP_PollSMS:			/* Our phone is able to notify us */
+	case GOP_PollSMS:			/* Our phone is able to notify us... but we do not want to burn 100% CPU polling */
+		{
+			struct timeval tm;
+			tm.tv_sec = 0;
+			tm.tv_usec = 50000;
+			device_select(&tm);
+		}
+		break;
 	case GOP_PollDisplay:
 		break;
 	default:
