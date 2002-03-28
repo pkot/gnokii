0edaf538a6ec7a5f6f760d3713910bb2e0fda0ac
pkot <pkot>
2002-03-28 21:37:43 +0000

    Updates to address GNU GPL requirements

--- common/gsm-sms.c
+++ common/gsm-sms.c
@@ -6,9 +6,23 @@
 
   A Linux/Unix toolset and driver for Nokia mobile phones.
 
-  Copyright (C) 2001 Pawe³ Kot <pkot@linuxnews.pl>
+  This file is part of gnokii.
 
-  Released under the terms of the GNU GPL, see file COPYING for more details.
+  Gnokii is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Gnokii is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with gnokii; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  Copyright (C) 2001 Pawe³ Kot <pkot@linuxnews.pl>
 
   Library for parsing and creating Short Messages (SMS).
 
@@ -1052,6 +1066,24 @@ static GSM_Error DecodeSMSHeader(unsigned char *message, GSM_SMSMessage *SMS)
 #ifdef DEBUG
 		dprintf("\tDelivery date: %s\n", PrintDateTime(message + llayout.Time));
 #endif
+	}
+
+	/* Remote number */
+	if (llayout.RemoteNumber > -1) {
+		if (llayout.IsRemoteNumberCoded) {
+			if (message[llayout.RemoteNumber] == 0x00) llayout.MessageCenter -= 4;
+				/* FIXME Is this an ugly hack or correct? */
+				/* at least it works with 6210, 6510 and 6110 with the message I tested */
+			message[llayout.RemoteNumber] = (message[llayout.RemoteNumber] + 1) / 2 + 1;
+			strcpy(SMS->RemoteNumber.number, GetBCDNumber(message + llayout.RemoteNumber));
+			dprintf("\tRemote number (recipient or sender): %s\n", SMS->RemoteNumber.number);
+		} else {
+			/* SMS struct should be zeroed for now, so there's no
+			 * need to add an extra '\0' at the end of the string */
+			strncpy(SMS->RemoteNumber.number,
+				message + 1 + llayout.RemoteNumber,
+				message[llayout.RemoteNumber] < GSM_MAX_SMS_CENTER_LENGTH ? message[llayout.RemoteNumber] : GSM_MAX_SMS_CENTER_LENGTH);
+		}
 	}
 
 	/* Remote number */
