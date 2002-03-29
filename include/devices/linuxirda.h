c979a19e04461ba6b1a8cac70943ed0c79dc7424
hugh <hugh>
2002-03-29 09:27:44 +0000

    Fixed small typo in .../linuxirda.h

--- include/devices/linuxirda.h
+++ include/devices/linuxirda.h
@@ -1,4 +1,4 @@
- * This file is part of gnokii.
+/* This file is part of gnokii.
  *
  * Gnokii is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
