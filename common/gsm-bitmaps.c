a1c5d6f94b72350c84617826bba4ccab1a9885ff
plail <plail>
2002-04-02 14:06:59 +0000

    Support for 6310/6510 operator logos (partially) added

--- common/gsm-bitmaps.c
+++ common/gsm-bitmaps.c
@@ -64,17 +64,29 @@ bool GSM_IsPointBitmap(GSM_Bitmap *bmp, int x, int y)
 	int i = 0;
 
 
-	if (bmp->type == GSM_OperatorLogo || bmp->type == GSM_CallerLogo)
+	if (bmp->type == GSM_OperatorLogo || bmp->type == GSM_CallerLogo){
 		i = (bmp->bitmap[(y*bmp->width+x)/8] & 1 << (7-((y*bmp->width+x)%8)));
+		if (i) 
+			return true;
+		else 
+			return false;
+	}
 
-	if (bmp->type == SMS_Picture) 
+	if (bmp->type == SMS_Picture) {
 		i = (bmp->bitmap[9 * y + (x / 8)] & 1 << (7 - (x % 8)));
+		if (i) 
+			return true;
+		else 
+			return false;
+	}
 
-	if ((bmp->type == GSM_StartupLogo) || (bmp->type == GSM_NewOperatorLogo))
+	if ((bmp->type == GSM_StartupLogo) || (bmp->type == GSM_NewOperatorLogo)){
 		i = (bmp->bitmap[((y/8)*bmp->width) + x] & 1<<((y%8)));
-
-	if (i) return true;
-	else return false;
+		if (i) 
+			return true;
+		else 
+			return false;
+	}
 }
 
 void GSM_ClearBitmap(GSM_Bitmap *bmp)
