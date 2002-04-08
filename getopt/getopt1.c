2e859b9711103a653078a317d223255db5df9d44
pkot <pkot>
2002-04-08 08:17:21 +0000

    Add missin comma (Feico de Boer)

--- getopt/getopt1.c
+++ getopt/getopt1.c
@@ -60,7 +60,7 @@ char *getenv();
 #endif
 
 int
-getopt_long(int argc, char *const *argv, const char *options
+getopt_long(int argc, char *const *argv, const char *options,
 	    const struct option *long_options, int *opt_index)
 {
 	return _getopt_internal(argc, argv, options, long_options, opt_index, 0);
