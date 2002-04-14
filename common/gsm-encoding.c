9345decb18cd706dedd206bfc9524017bbb71f1e
pkot <pkot>
2002-04-14 16:22:37 +0000

    Fix the problem with encoding international numbers

--- common/gsm-encoding.c
+++ common/gsm-encoding.c
@@ -379,7 +379,7 @@ char *GetBCDNumber(u8 *Number)
 		break;
 	case SMS_International:
 		sprintf(Buffer, "+");
-		length--;
+		if (length == MAX_BCD_STRING_LENGTH) length--; /* avoid overflow */
 	case SMS_Unknown:
 	case SMS_National:
 	case SMS_Network:
