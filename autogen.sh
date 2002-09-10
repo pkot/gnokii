ed46ace2a1c093dc6a8ad11e764cb86a105cfee9
pkot <pkot>
2002-09-10 20:19:01 +0000

    Workaround for the gettext used in RH 7.3 Expect better solution soon. (Mariusz Mazur)

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,9 @@
 
 # comment the next line when you have the system without NLS
 gettextize -c -f
+if [ -f po/Makevars.template ]; then
+	mv po/Makevars.template po/Makevars
+fi
 aclocal
 autoheader
 autoconf
