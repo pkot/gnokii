d696ce5087a9b4790034da6d478be59b6e305ac0
pkot <pkot>
2002-11-01 23:23:20 +0000

    Entry location was pronted instead of the group id in the gnokii.c::getphonebook() (Ruotger Skupin)

--- gnokii/gnokii.c
+++ gnokii/gnokii.c
@@ -3012,7 +3012,7 @@ static int getphonebook(int argc, char *argv[])
 				sprintf(buf, "X_GSM_STORE_AT:%s%d\n", memory_type_string, entry.Location);
 				phonebook2vcard(stdout, &entry, buf);
 			} else {
-				fprintf(stdout, _("%d. Name: %s\nNumber: %s\nGroup id: %d\n"), entry.Location, entry.Name, entry.Number, entry.Location);
+				fprintf(stdout, _("%d. Name: %s\nNumber: %s\nGroup id: %d\n"), entry.Location, entry.Name, entry.Number, entry.Group);
 				for (i = 0; i < entry.SubEntriesCount; i++) {
 					switch (entry.SubEntries[i].EntryType) {
 					case 0x08:
