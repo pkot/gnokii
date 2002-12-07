39b06f8c6373cc8ba5a8b2c9379d343160acc3af
pkot <pkot>
2002-12-07 13:03:31 +0000

    Small changes to autogen.sh to work harmfully with new getextize

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # comment the next line when you have the system without NLS
-gettextize -c -f
+gettextize -c -f --intl --no-changelog
+
+# I don't like when this file gets automatically changed
+if [ -f configure.in~ ]; then
+	mv configure.in~ configure.in
+fi
+
 if [ -f po/Makevars.template ]; then
 	mv po/Makevars.template po/Makevars
 fi
