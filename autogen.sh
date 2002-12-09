7862c156f06043cb0d0bfaa3dab1dd2d53676606
pkot <pkot>
2002-12-09 23:19:55 +0000

    Cleanups in the gettext support

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # comment the next line when you have the system without NLS
-gettextize -c -f --intl --no-changelog
+if ! gettextize --version | grep -q '0\.10\.' ; then
+	gettextize -c -f --intl --no-changelog
+else
+	gettextize -c -f
+fi
 
 # I don't like when this file gets automatically changed
 if [ -f configure.in~ ]; then
 	mv configure.in~ configure.in
 fi
 
-if [ -f po/Makevars.template ]; then
-	mv po/Makevars.template po/Makevars
-fi
 aclocal
 autoheader
 autoconf
