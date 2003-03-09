b4d26cf963b7ecfd4f3d0970db74d6f4c7f2a315
pkot <pkot>
2003-03-09 13:37:29 +0000

    Fix DLR3P support for nk7110 driver

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -360,9 +360,9 @@ static gn_error NK6510_Initialise(struct gn_statemachine *state)
 {
 	gn_data data;
 	char model[10];
-	gn_error err;
+	gn_error err = GN_ERR_NONE;
 	bool connected = false;
-	int attempt = 0;
+	unsigned int attempt = 0;
 
 	/* Copy in the phone info */
 	memcpy(&(state->driver), &driver_nokia_6510, sizeof(gn_driver));
@@ -379,7 +379,10 @@ static gn_error NK6510_Initialise(struct gn_statemachine *state)
 				break;
 			}
 		case GN_CT_Serial:
+			err = fbus_initialise(attempt++, state);
+			break;
 		case GN_CT_Bluetooth:
+			attempt = 2;
 			err = fbus_initialise(attempt++, state);
 			break;
 		case GN_CT_Infrared:
