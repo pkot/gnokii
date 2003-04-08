bdac86b5cc7cfd189925d0c9fdf33164a2274d16
pkot <pkot>
2003-04-08 21:53:31 +0000

    Get rid of HAAVE_BLUETOOTH in the exported header

--- include/gnokii/data.h
+++ include/gnokii/data.h
@@ -35,7 +35,7 @@
 #include <gnokii/error.h>
 #include <gnokii/rlp-common.h>
 
-#ifdef HAVE_BLUETOOTH
+#ifdef @HAVE_BLUETOOTH_EXP@
 #  include <bluetooth/bluetooth.h>
 #endif
 
@@ -130,7 +130,7 @@ typedef struct {
 	int smsc_timeout;				/* How many seconds should we wait for the SMSC response, defaults to 10 seconds */
 	char connect_script[256];			/* Script to run when device connection established */
 	char disconnect_script[256];			/* Script to run when device connection closed */
-#ifdef HAVE_BLUETOOTH
+#ifdef @HAVE_BLUETOOTH_EXT@
 	uint8_t rfcomm_cn;				/* RFCOMM channel number to connect */
 	bdaddr_t bt_address;				/* Bluetooth device address */
 #endif
