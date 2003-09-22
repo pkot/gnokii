1112b4c4eaceb01f82d8237417370d9da8a663e3
pkot <pkot>
2003-09-22 20:04:52 +0000

    win32 update: fix compilation warnings

--- common/gsm-encoding.c
+++ common/gsm-encoding.c
@@ -556,9 +556,10 @@ int utf8_decode(char *outstring, const char *instring, int inlen)
 	int outlen = inlen;
 #ifdef HAVE_ICONV
 	iconv_t cd;
+	char *iter = instring;
 
 	cd = iconv_open(nl_langinfo(CODESET), "UTF-8");
-	outlen = iconv(cd, &instring, (size_t *)&inlen, &outstring, (size_t *)&outlen);
+	outlen = iconv(cd, &iter, (size_t *)&inlen, &outstring, (size_t *)&outlen);
 	iconv_close(cd);
 	outstring[outlen] = 0;
 #endif
@@ -572,9 +573,10 @@ int utf8_encode(char *outstring, const char *instring, int inlen)
 	int len = 0;
 #ifdef HAVE_ICONV
 	iconv_t cd;
+	char *iter = instring;
 
 	cd = iconv_open("UTF-8", nl_langinfo(CODESET));
-	len = iconv(cd, &instring, &inlen, &outstring, &outlen);
+	len = iconv(cd, &iter, &inlen, &outstring, &outlen);
 	iconv_close(cd);
 #endif
 	if (len < 0) return -1;
