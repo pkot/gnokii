87122af76970bc4141292b182f5fced4e137f742
pkot <pkot>
2004-04-25 16:00:04 +0000

    Fix a comment

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -1510,7 +1510,7 @@ static gn_error NK6510_IncomingPhonebook(int messagetype, unsigned char *message
 			switch (message[10]) {
 			case 0x0f: return GN_ERR_WRONGDATAFORMAT; /* I got this when sending incorrect
 								     block (with 0 length) */
-			case 0x23: return GN_ERR_WRONGDATAFORMAT; /* ';' in the name */
+			case 0x23: return GN_ERR_WRONGDATAFORMAT; /* block size does not match a definition */
 			case 0x36: return GN_ERR_WRONGDATAFORMAT; /* name block is too long */
 			case 0x3d: return GN_ERR_FAILED;
 			case 0x3e: return GN_ERR_FAILED;
