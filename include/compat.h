db4b30175345f8d22f676f9994bff0b5b10c6a80
pkot <pkot>
2004-06-27 20:36:16 +0000

    Add int types defined in stdint.h or inttypes.h on systems without these headers

--- include/compat.h
+++ include/compat.h
@@ -243,6 +243,11 @@ int vasprintf(char **ptr, const char *format, va_list ap);
 /* for Linux Bluetooth compability */
 #if !defined(HAVE_STDINT_H) && !defined(HAVE_INTTYPES_H)
 	typedef unsigned char uint8_t;
+	typedef unsigned short uint16_t;
+	typedef unsigned int uint32_t;
+	typedef char int8_t;
+	typedef short int16_t;
+	typedef int int32_t;
 #endif
 
 #ifdef HAVE_WCRTOMB
