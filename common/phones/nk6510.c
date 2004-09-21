d9060be4357f20cf8adbb1eb9876ec13d0d11352
bozo <bozo>
2004-09-21 00:02:58 +0000

    clear empty flag when phonebook entry read

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -1507,6 +1507,9 @@ static gn_error NK6510_IncomingPhonebook(int messagetype, unsigned char *message
 			}
 		}
 		dprintf("Received phonebook info\n");
+		if (data->phonebook_entry) {
+			data->phonebook_entry->empty = false;
+		}
 		blocks     = message[21];
 		return phonebook_decode(message + 22, length - 21, data, blocks, message[11], 12);
 	case 0x0c: /* Write memory location */
