37c896a7ca96fcdd3c5533ee453936058a9fa8af
pkot <pkot>
2006-01-26 23:44:48 +0000

    These strings should not be translatable

--- xgnokii/xgnokii_logos.c
+++ xgnokii/xgnokii_logos.c
@@ -1273,8 +1273,8 @@ gint ToolTypeEvent(GtkWidget * widget)
 gint LogoTypeEvent(GtkWidget * widget)
 {
 	int clear = 0;
-	g_print(_("LogoTypeEvent called!\n"));
-	g_print(_("width: %i, height: %i\n"), statemachine.driver.phone.startup_logo_width,
+	g_print("LogoTypeEvent called!\n");
+	g_print("width: %i, height: %i\n", statemachine.driver.phone.startup_logo_width,
 		statemachine.driver.phone.startup_logo_height);
 	/* is startupLogo? */
 	/* Resize and clear anyway - CK */
