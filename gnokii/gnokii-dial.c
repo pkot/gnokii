ed68e6af66f4da096eafbea95bf52a55061254c7
dforsi <dforsi>
2006-11-01 12:43:48 +0000

    make --getspeeddial and --setspeeddial print to stdout on success, not stderr

--- gnokii/gnokii-dial.c
+++ gnokii/gnokii-dial.c
@@ -80,7 +80,7 @@ int getspeeddial(char *number, gn_data *data, struct gn_statemachine *state)
 
 	switch (error) {
 	case GN_ERR_NONE:
-		fprintf(stderr, _("SpeedDial nr. %d: %d:%d\n"), speeddial.number, speeddial.memory_type, speeddial.location);
+		fprintf(stdout, _("SpeedDial nr. %d: %d:%d\n"), speeddial.number, speeddial.memory_type, speeddial.location);
 		break;
 	default:
 		fprintf(stderr, _("Error: %s\n"), gn_error_print(error));
@@ -119,7 +119,7 @@ int setspeeddial(char *argv[], gn_data *data, struct gn_statemachine *state)
 
 	switch (error) {
 	case GN_ERR_NONE:
-		fprintf(stderr, _("Successfully written!\n"));
+		fprintf(stdout, _("Successfully written!\n"));
 		break;
 	default:
 		fprintf(stderr, _("Error: %s\n"), gn_error_print(error));
