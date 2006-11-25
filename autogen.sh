393732cfaa8aa676bb87ee3d3ce41ae6ef97022b
pkot <pkot>
2006-11-25 20:07:43 +0000

    Replace gettextize with autopoint

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-# comment the next line when you have the system without NLS
-if ! gettextize --version | grep -q '0\.10\.' ; then
-	gettextize -c -f --intl --no-changelog
-else
-	gettextize -c -f
-fi
-
-# I don't like when this file gets automatically changed
-if [ -f configure.in~ ]; then
-	mv configure.in~ configure.in
-fi
-
+autopoint -f
 libtoolize -c -f
 AC_LOCAL_FLAGS="-I m4"
 if [ "`uname -s`"x = "FreeBSD"x ]; then
