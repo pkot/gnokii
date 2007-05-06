7c25788edadd97f57e982a6be66894fe9ea19752
pkot <pkot>
2007-05-06 16:42:31 +0000

    --monitor delay requires an argument, not --monitor once

--- gnokii/gnokii-monitor.c
+++ gnokii/gnokii-monitor.c
@@ -220,7 +220,7 @@ int monitormode(int argc, char *argv[], gn_data *data, struct gn_statemachine *s
 	if (argc > optind) {
 		d = !strcasecmp(argv[optind], "once") ? -1 : gnokii_atoi(argv[optind]);
 		if (errno) {
-			fprintf(stderr, _("Argument to --monitor once needs to be positive number\n"));
+			fprintf(stderr, _("Argument to --monitor delay needs to be positive number\n"));
 			return -1;
 		}
 	} else
