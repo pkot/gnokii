9bd0f6912930d0eb7d0dddbae431aaf7367836cc
pkot <pkot>
2007-06-03 20:24:32 +0000

    Fix typo

--- include/gnokii.h
+++ include/gnokii.h
@@ -65,7 +65,7 @@ typedef unsigned char uint8_t;
 #endif
 
 /* if you change those numbers, change them in common/Makefile as well! */
-#define LIBGNOKII_VERSION_STRING "5.3.0"
+#define LIBGNOKII_VERSION_STRING "5.2.0"
 #define LIBGNOKII_VERSION_MAJOR 5
 #define LIBGNOKII_VERSION_MINOR 2
 #define LIBGNOKII_VERSION_RELEASE 0
