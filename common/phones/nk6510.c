e11f34a8d7c4df992124a733de6f8515497c3e4f
pkot <pkot>
2007-07-09 20:15:47 +0000

    Treat Postal entries as addresses (Keith Packard)

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -2361,6 +2361,7 @@ static gn_error NK6510_WritePhonebookLocation(gn_data *data, struct gn_statemach
 			for (i = 0; i < entry->subentries_count; i++)
 				switch (entry->subentries[i].entry_type) {
 				case GN_PHONEBOOK_ENTRY_ExtendedAddress:
+				case GN_PHONEBOOK_ENTRY_Postal:
 				case GN_PHONEBOOK_ENTRY_Street:
 				case GN_PHONEBOOK_ENTRY_City:
 				case GN_PHONEBOOK_ENTRY_StateProvince:
@@ -2419,6 +2420,7 @@ static gn_error NK6510_WritePhonebookLocation(gn_data *data, struct gn_statemach
 				for (i = 0; i < entry->subentries_count; i++) {
 					switch (entry->subentries[i].entry_type) {
 					case GN_PHONEBOOK_ENTRY_ExtendedAddress:
+					case GN_PHONEBOOK_ENTRY_Postal:
 					case GN_PHONEBOOK_ENTRY_Street:
 					case GN_PHONEBOOK_ENTRY_City:
 					case GN_PHONEBOOK_ENTRY_StateProvince:
