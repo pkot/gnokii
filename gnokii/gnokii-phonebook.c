552c228ea90174f6d4594ad60c2ff7cb14fc37af
dforsi <dforsi>
2007-11-04 11:22:56 +0000

    do not count empty entries when driver returns GN_ERR_NONE;
    when "end" keyword is specified do not read past the last location according to GN_OP_GetMemoryStatus (this only avoids reading one location, the first that would have returned GN_ERR_INVALIDLOCATION, but it is a cheap optimization);
    
    num_entries depends on start_entry only if all locations below start_entry are full, but we can't know if they are full since we don't read them, so remove this optimization for now;

--- gnokii/gnokii-phonebook.c
+++ gnokii/gnokii-phonebook.c
@@ -145,7 +145,8 @@ int getphonebook(int argc, char *argv[], gn_data *data, struct gn_statemachine *
 	if (end_entry == INT_MAX) {
 		data->memory_status = &memstat;
 		if ((error = gn_sm_functions(GN_OP_GetMemoryStatus, data, state)) == GN_ERR_NONE) {
-			num_entries = memstat.used - start_entry + 1;
+			num_entries = memstat.used;
+			end_entry = memstat.used + memstat.free;
 		}
 	}
 
@@ -162,9 +163,9 @@ int getphonebook(int argc, char *argv[], gn_data *data, struct gn_statemachine *
 		switch (error) {
 			int i;
 		case GN_ERR_NONE:
-			num_entries--;
 			if (entry.empty != false)
 				break;
+			num_entries--;
 			switch (type) {
 			case 1:
 				gn_file_phonebook_raw_write(stdout, &entry, memory_type_string);
