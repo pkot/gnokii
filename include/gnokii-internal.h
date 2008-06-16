480607ef18a5eaf8d4593f70364a83b3446d8868
hadess <hadess>
2008-06-16 14:48:24 +0000

    Make the 2nd argument to strip_slashes a const, as we don't modify it in the function

--- include/gnokii-internal.h
+++ include/gnokii-internal.h
@@ -171,6 +171,6 @@ int utf8_base64_decode(char *dest, int destlen, const char *src, int inlen);
 int utf8_base64_encode(char *dest, int destlen, const char *src, int inlen);
 
 int add_slashes(char *dest, char *src, int maxlen, int len);
-int strip_slashes(char *dest, char *src, int maxlen, int len);
+int strip_slashes(char *dest, const char *src, int maxlen, int len);
 
 #endif /* _gnokii_internal_h */
