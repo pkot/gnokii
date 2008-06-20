f7493b6161b16238dfabdcb64d968791a8019656
hadess <hadess>
2008-06-20 15:33:33 +0000

    Fix another condition being the wrong way around, fix a strstr wrong arg Thanks Daniele

--- common/vcard.c
+++ common/vcard.c
@@ -282,7 +282,7 @@ GNOKII_API int gn_vcard2phonebook(FILE *f, gn_phonebook_entry *entry)
 	}
 
 	str_append_printf (&str, "BEGIN:VCARD");
-	while (!fgets(buf, 1024, f)) {
+	while (fgets(buf, 1024, f)) {
 		str_append_printf (&str, buf);
 		if (BEGINS("END:VCARD"))
 			break;
@@ -313,7 +313,7 @@ GNOKII_API int gn_vcardstr2phonebook(const char *vcard, gn_phonebook_entry *entr
 	fold = strstr (v, "\r\n");
 	while (fold != NULL) {
 		memmove (fold, fold + 2, strlen (fold) - 2);
-		fold = strstr (v, "\r\n");
+		fold = strstr (fold, "\r\n");
 	}
 
 	/* Count the number of lines */
