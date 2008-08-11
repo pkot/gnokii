b11e4ff7df157ecb61e5957f156c233cbbe2f056
dforsi <dforsi>
2008-08-11 14:55:46 +0000

    check that the cell id has at least 8 chars (every 4-chars cell id verifies the condition if (strlen(pos) > 4) when the trailing quotes aren't stripped)
    fixes invalid reads reported by valgrind:
    
    ==17209== Invalid read of size 1
    ==17209==    at 0x80996A1: creg_parse (atgen.c:2469)
    ==17209==    by 0x809D821: ReplyGetNetworkInfo (atgen.c:2534)
    ==17209==    by 0x805F9FB: sm_incoming_function (gsm-statemachine.c:131)
    ==17209==    by 0x80A9761: atbus_loop (atbus.c:238)
    ==17209==    by 0x805FB3F: gn_sm_loop (gsm-statemachine.c:84)
    ==17209==    by 0x805FDE4: __sm_block_timeout (gsm-statemachine.c:275)
    ==17209==    by 0x805FEB6: sm_block_no_retry (gsm-statemachine.c:317)
    ==17209==    by 0x80989D7: AT_GetNetworkInfo (atgen.c:1391)
    ==17209==    by 0x809AEA6: Functions (atgen.c:293)
    ==17209==    by 0x8053149: getnetworkinfo (gnokii-other.c:189)
    ==17209==    by 0x804C019: parse_options (gnokii.c:1035)
    ==17209==    by 0x804BC44: parse_options (gnokii.c:772)
    ==17209==  Address 0x44b9c3f is 0 bytes after a block of size 7 alloc'd
    ==17209==    at 0x4021AB8: malloc (vg_replace_malloc.c:207)
    ==17209==    by 0x4284FCF: strdup (in /lib/tls/i686/cmov/libc-2.7.so)
    ==17209==    by 0x807834B: gnokii_strsplit (misc.c:596)
    ==17209==    by 0x809D7F4: ReplyGetNetworkInfo (atgen.c:2515)
    ==17209==    by 0x805F9FB: sm_incoming_function (gsm-statemachine.c:131)
    ==17209==    by 0x80A9761: atbus_loop (atbus.c:238)
    ==17209==    by 0x805FB3F: gn_sm_loop (gsm-statemachine.c:84)
    ==17209==    by 0x805FDE4: __sm_block_timeout (gsm-statemachine.c:275)
    ==17209==    by 0x805FEB6: sm_block_no_retry (gsm-statemachine.c:317)
    ==17209==    by 0x80989D7: AT_GetNetworkInfo (atgen.c:1391)
    ==17209==    by 0x809AEA6: Functions (atgen.c:293)
    ==17209==    by 0x8053149: getnetworkinfo (gnokii-other.c:189)
    ==17209==
    ==17209== Invalid read of size 1
    ==17209==    at 0x80996A8: creg_parse (atgen.c:2470)
    ==17209==    by 0x809D821: ReplyGetNetworkInfo (atgen.c:2534)
    ==17209==    by 0x805F9FB: sm_incoming_function (gsm-statemachine.c:131)
    ==17209==    by 0x80A9761: atbus_loop (atbus.c:238)
    ==17209==    by 0x805FB3F: gn_sm_loop (gsm-statemachine.c:84)
    ==17209==    by 0x805FDE4: __sm_block_timeout (gsm-statemachine.c:275)
    ==17209==    by 0x805FEB6: sm_block_no_retry (gsm-statemachine.c:317)
    ==17209==    by 0x80989D7: AT_GetNetworkInfo (atgen.c:1391)
    ==17209==    by 0x809AEA6: Functions (atgen.c:293)
    ==17209==    by 0x8053149: getnetworkinfo (gnokii-other.c:189)
    ==17209==    by 0x804C019: parse_options (gnokii.c:1035)
    ==17209==    by 0x804BC44: parse_options (gnokii.c:772)
    ==17209==  Address 0x44b9c40 is 1 bytes after a block of size 7 alloc'd
    ==17209==    at 0x4021AB8: malloc (vg_replace_malloc.c:207)
    ==17209==    by 0x4284FCF: strdup (in /lib/tls/i686/cmov/libc-2.7.so)
    ==17209==    by 0x807834B: gnokii_strsplit (misc.c:596)
    ==17209==    by 0x809D7F4: ReplyGetNetworkInfo (atgen.c:2515)
    ==17209==    by 0x805F9FB: sm_incoming_function (gsm-statemachine.c:131)
    ==17209==    by 0x80A9761: atbus_loop (atbus.c:238)
    ==17209==    by 0x805FB3F: gn_sm_loop (gsm-statemachine.c:84)
    ==17209==    by 0x805FDE4: __sm_block_timeout (gsm-statemachine.c:275)
    ==17209==    by 0x805FEB6: sm_block_no_retry (gsm-statemachine.c:317)
    ==17209==    by 0x80989D7: AT_GetNetworkInfo (atgen.c:1391)
    ==17209==    by 0x809AEA6: Functions (atgen.c:293)
    ==17209==    by 0x8053149: getnetworkinfo (gnokii-other.c:189)

--- common/phones/atgen.c
+++ common/phones/atgen.c
@@ -2460,7 +2460,7 @@ static gn_error creg_parse(char **strings, int i, gn_network_info *ninfo, int la
 	ninfo->cell_id[1] = strtol(tmp, NULL, 16);
 
 	/* Ugly, ugly... */
-	if (strlen(pos) > 4) {
+	if (strlen(pos) >= 8) {
 		tmp[0] = pos[4];
 		tmp[1] = pos[5];
 
