2a7df0a30aca1083f3267cbcddcf03ccd464644f
dforsi <dforsi>
2008-09-10 12:32:01 +0000

    fix dgettext() compatibility macro for when compiling without NLS support (needs to accept two arguments and ignore the first one)

--- include/compat.h
+++ include/compat.h
@@ -257,7 +257,7 @@ time_t timegm(struct tm *tm);
 #else
 #  define _(x) (x)
 #  define N_(x) (x)
-#  define dgettext(x) (x)
+#  define dgettext(y, x) (x)
 #endif /* ENABLE_NLS */
 
 /* Definitions for u8, u16 and u32, borrowed from
