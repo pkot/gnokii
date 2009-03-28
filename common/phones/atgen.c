19b89a77f48211d67d9e9836e12c9756fdc51915
pkot <pkot>
2009-03-28 21:45:12 +0000

    if AT+GMM fails try AT+CGMM (Huawei E220 doesn't support +GMM but supports +CGMM); fixes #26039

--- common/phones/atgen.c
+++ common/phones/atgen.c
@@ -867,13 +867,23 @@ static gn_error AT_Identify(gn_data *data, struct gn_statemachine *state)
 
 static gn_error AT_GetModel(gn_data *data, struct gn_statemachine *state)
 {
-	/* prefer AT+GMM over AT+CGMM because it returns a user friendly model name
-	   for some phones (e.g. Sony Ericsson Z310i)
+	gn_error err;
+	/*
+	 * prefer AT+GMM over AT+CGMM because it returns a user friendly model name
+	 * for some phones (e.g. Sony Ericsson Z310i)
 	 */
 
 	if (sm_message_send(7, GN_OP_Identify, "AT+GMM\r", state))
 		return GN_ERR_NOTREADY;
-	return sm_block_no_retry(GN_OP_Identify, data, state);
+	if ((err = sm_block_no_retry(GN_OP_Identify, data, state)) == GN_ERR_NONE)
+		return GN_ERR_NONE;
+
+	if (sm_message_send(8, GN_OP_Identify, "AT+CGMM\r", state))
+		return GN_ERR_NOTREADY;
+	if ((err = sm_block_no_retry(GN_OP_Identify, data, state)) == GN_ERR_NONE)
+		return GN_ERR_NONE;
+
+	return err;
 }
 
 static gn_error AT_GetManufacturer(gn_data *data, struct gn_statemachine *state)
