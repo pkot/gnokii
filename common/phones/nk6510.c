c8ad4919539f585437e289149b07de0cb8f34de7
Pawel Kot <gnokii@gmail.com>
2009-11-08 19:58:08 +0100

    Make the file compile.

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -2417,11 +2417,11 @@ static gn_error NK6510_GetFileById(gn_data *data, struct gn_statemachine *state)
 		if (err != GN_ERR_NONE)
 			return err;
 		progress = 100 * (data->file->file_length - data->file->togo) / data->file->file_length;
-		if (!DRVINSTANCE(state)->progress_indication)
+		if (!DRVINSTANCE(state)->progress_indication) {
 			fprintf(stderr, _("Progress: %3d%% completed"), progress);
 			/* It's separated for the translators convenience */
 			fprintf(stderr, "\r");
-		else
+		} else
 			DRVINSTANCE(state)->progress_indication(progress, DRVINSTANCE(state)->progress_callback_data);
 	}
 	if (!DRVINSTANCE(state)->progress_indication)
