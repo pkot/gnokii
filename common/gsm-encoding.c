pkot/gnokii

commit 8c58724c842cf4c042b2cc060d77958ae34a8d58

According to 3GPP TS 23.040, version 6.7.0, section 9.1.2.5, page 39-41, GSM number type is just a mask on bits 4-6 of the given octet rather than fully blown octet define. Therfore when parsing the received message we should not compare the octed but only the masked value.

common/gsm-encoding.c
@@ -1,7 +1,5 @@
 /*
 
-  $Id$
-
   G N O K I I
 
   A Linux/Unix toolset and driver for the mobile phones.
@@ -23,7 +21,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
   Copyright (C) 1999-2000 Pavel Janik ml.
-  Copyright (C) 2001-2005 Pawel Kot
+  Copyright (C) 2001-2011 Pawel Kot
   Copyright (C) 2002      Markus Plail, Manfred Jonsson
   Copyright (C) 2002-2004 BORBELY Zoltan
   Copyright (C) 2003      Martin Goldhahn
@@ -1083,12 +1081,14 @@ int char_semi_octet_pack(char *number, unsigned char *output, gn_gsm_number_type
 
 	*out_num++ = type;
 
-	if (type == GN_GSM_NUMBER_Alphanumeric) {
+	if (((type & GN_GSM_NUMBER_Type_Mask) & GN_GSM_NUMBER_Alphanumeric_Mask) == GN_GSM_NUMBER_Alphanumeric_Mask) {
 		count = strlen(number);
 		return 2 * char_7bit_pack(0, number, out_num, &count);
 	}
 
-	if ((type == GN_GSM_NUMBER_International || type == GN_GSM_NUMBER_Unknown) && *in_num == '+')
+	if (((((type & GN_GSM_NUMBER_Type_Mask) & GN_GSM_NUMBER_International_Mask) == GN_GSM_NUMBER_International) ||
+		(type & GN_GSM_NUMBER_Type_Mask) == 0) && /* Unknown */
+		*in_num == '+')
 		in_num++; /* skip leading '+' */
 
 	/* The next field is the number. It is in semi-octet representation - see

include/gnokii/common.h
@@ -661,8 +661,33 @@ typedef struct {
 
 #define GN_BCD_STRING_MAX_LENGTH 40
 
-/* This data-type is used to specify the type of the number. See the official
-   GSM specification 03.40, version 6.1.0, section 9.1.2.5, page 35-37. */
+/*
+ * This data-type is used to specify the type of the number. See the official
+ * GSM specification 03.40, version 6.1.0, section 9.1.2.5, page 35-37 (old)
+ * 3GPP TS 23.040, version 6.7.0, section 9.1.2.5, page 39-41 (new)
+ */
+
+/* bit 7 is always 1 */
+#define GN_GSM_NUMBER_Type_Mask			0x70
+
+#define GN_GSM_NUMBER_International_Mask	0x10
+#define GN_GSM_NUMBER_National_Mask		0x20
+#define GN_GSM_NUMBER_Network_Mask		0x30
+#define GN_GSM_NUMBER_Subecriber_Mask		0x40
+#define GN_GSM_NUMBER_Alphanumeric_Mask		0x50
+#define GN_GSM_NUMBER_Abbreviated_Mask		0x60
+
+#define GN_GSM_NUMBER_Numbering_Plan_Mask	0x0f
+
+#define GN_GSM_NUMBER_ISDN_Plan_Mask		0x01
+#define GN_GSM_NUMBER_Data_Plan_Mask		0x03
+#define GN_GSM_NUMBER_Telex_Plan_Mask		0x04
+#define GN_GSM_NUMBER_Specific1_Plan_Mask	0x05
+#define GN_GSM_NUMBER_Specific2_Plan_Mask	0x06
+#define GN_GSM_NUMBER_National_Plan_Mask	0x08
+#define GN_GSM_NUMBER_Private_Plan_Mask		0x09
+#define GN_GSM_NUMBER_ERMES_Plan_Mask		0x0a
+
 typedef enum {
 	GN_GSM_NUMBER_Unknown       = 0x81, /* Unknown number */
 	GN_GSM_NUMBER_International = 0x91, /* International number */

commit c563bb984165620365e547b0c983f756e7461dff

Fix incorrect encoding of international numbers caused by 8c58724c842cf4c042b2cc060d77958ae34a8d58.

common/gsm-encoding.c
@@ -1081,12 +1081,13 @@ int char_semi_octet_pack(char *number, unsigned char *output, gn_gsm_number_type
 
 	*out_num++ = type;
 
+	
 	if (((type & GN_GSM_NUMBER_Type_Mask) & GN_GSM_NUMBER_Alphanumeric_Mask) == GN_GSM_NUMBER_Alphanumeric_Mask) {
 		count = strlen(number);
 		return 2 * char_7bit_pack(0, number, out_num, &count);
 	}
 
-	if (((((type & GN_GSM_NUMBER_Type_Mask) & GN_GSM_NUMBER_International_Mask) == GN_GSM_NUMBER_International) ||
+	if ((((type & GN_GSM_NUMBER_International_Mask) == GN_GSM_NUMBER_International_Mask) ||
 		(type & GN_GSM_NUMBER_Type_Mask) == 0) && /* Unknown */
 		*in_num == '+')
 		in_num++; /* skip leading '+' */