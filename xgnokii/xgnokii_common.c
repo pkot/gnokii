be15ca63bcc34c82bebf9b1063528647786724d8
Pawel Kot <gnokii@gmail.com>
2020-01-18 22:30:33 +0100

    Cleanup usage of compat.h and config.h
    compat.h is there to simplify including common header files. If we
    need a number of standard header files we just include "compat.h" and
    it does everything for us. Therefore all standard headers includes
    are removed from the source files.
    
    In addition add conditional includes where we did the checks in
    config.h.
    
    Include config.h where needed.
    
    Add detection of error.h, errno.h and signal.h.

--- xgnokii/xgnokii_common.c
+++ xgnokii/xgnokii_common.c
@@ -16,9 +16,6 @@
 #ifdef HAVE_SIGNAL_H
 #  include <signal.h>
 #endif
-#ifdef HAVE_SYS_WAIT_H
-#  include <sys/wait.h>
-#endif
 #include <gtk/gtk.h>
 #include "misc.h"		/* for _() */
 #include "xgnokii_common.h"
