86f755d85a21edfe58dbd52694dc43f02288a12a
Ladislav Michl <ladis@linux-mips.org>
2021-11-09 07:27:28 +0100

    gen_gnokii_h.bat: use absolute paths
    Make batch file work no matter what directory it is called from.
    Also although batch files are case insensitive, be consistent.

--- Windows/gen_gnokii_h.bat
+++ Windows/gen_gnokii_h.bat
@@ -1,13 +1,13 @@
 @echo off
 
-set GNOKII_H_IN_PATH=..\include
+set GNOKII_H_IN_PATH=%~dp0\..\include
 set SED=sed.exe
 
-if not exist %GNOKII_H_IN_PATH%\gnokii.h.in goto :path_error
+if not exist "%GNOKII_H_IN_PATH%\gnokii.h.in" goto :path_error
 call :path_search %SED%
-if not exist %SED_PATH%%SED% goto :sed_error
+if not exist "%SED_PATH%%SED%" goto :sed_error
 
-findstr /r /c:"^GNOKII_LT_VERSION_" ..\configure.in > %TEMP%\gnokii_ver.txt
+findstr /r /c:"^GNOKII_LT_VERSION_" %~dp0\..\configure.ac > %TEMP%\gnokii_ver.txt
 
 for /F %%i in (%TEMP%\gnokii_ver.txt) do set %%i
 del /q %TEMP%\gnokii_ver.txt
@@ -25,7 +25,6 @@ set LIBGNOKII_VERSION_STRING=%LIBGNOKII_VERSION_MAJOR%.%LIBGNOKII_VERSION_MINOR%
 
 echo Ver: %LIBGNOKII_VERSION_STRING%
 
-
 rem include\gnokii.h.in has:
 rem #define LIBGNOKII_VERSION_STRING "@LIBGNOKII_VERSION_STRING@"
 rem #define LIBGNOKII_VERSION_MAJOR @LIBGNOKII_VERSION_MAJOR@
@@ -33,13 +32,13 @@ rem #define LIBGNOKII_VERSION_MINOR @LIBGNOKII_VERSION_MINOR@
 rem #define LIBGNOKII_VERSION_RELEASE @LIBGNOKII_VERSION_RELEASE@
 
 rem Prepare
-echo s/@LIBGNOKII_VERSION_STRING@/%LIBGNOKII_VERSION_STRING%/ > %temp%\gnokii_version.sed
-echo s/@LIBGNOKII_VERSION_MAJOR@/%LIBGNOKII_VERSION_MAJOR%/ >> %temp%\gnokii_version.sed
-echo s/@LIBGNOKII_VERSION_MINOR@/%LIBGNOKII_VERSION_MINOR%/ >> %temp%\gnokii_version.sed
-echo s/@LIBGNOKII_VERSION_RELEASE@/%LIBGNOKII_VERSION_RELEASE%/ >> %temp%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_STRING@/%LIBGNOKII_VERSION_STRING%/ > %TEMP%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_MAJOR@/%LIBGNOKII_VERSION_MAJOR%/ >> %TEMP%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_MINOR@/%LIBGNOKII_VERSION_MINOR%/ >> %TEMP%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_RELEASE@/%LIBGNOKII_VERSION_RELEASE%/ >> %TEMP%\gnokii_version.sed
 
 rem Execute sed with parameters
-sed -f %temp%\gnokii_version.sed %GNOKII_H_IN_PATH%\gnokii.h.in > %GNOKII_H_IN_PATH%\gnokii.h
+%SED% -f %TEMP%\gnokii_version.sed "%GNOKII_H_IN_PATH%\gnokii.h.in" > "%GNOKII_H_IN_PATH%\gnokii.h"
 del /q %TEMP%\gnokii_version.sed
 
 echo done!
