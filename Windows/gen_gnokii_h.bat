7ae0b162e6ee8a5431c236ecbb4cb7edafba4230
Ladislav Michl <ladis@linux-mips.org>
2021-11-09 07:27:28 +0100

    gen_gnokii_h.bat: unify case
    Although Batch files are case insensitive, be consistent.

--- Windows/gen_gnokii_h.bat
+++ Windows/gen_gnokii_h.bat
@@ -25,7 +25,6 @@ set LIBGNOKII_VERSION_STRING=%LIBGNOKII_VERSION_MAJOR%.%LIBGNOKII_VERSION_MINOR%
 
 echo Ver: %LIBGNOKII_VERSION_STRING%
 
-
 rem include\gnokii.h.in has:
 rem #define LIBGNOKII_VERSION_STRING "@LIBGNOKII_VERSION_STRING@"
 rem #define LIBGNOKII_VERSION_MAJOR @LIBGNOKII_VERSION_MAJOR@
@@ -33,13 +32,13 @@ rem #define LIBGNOKII_VERSION_MINOR @LIBGNOKII_VERSION_MINOR@
 rem #define LIBGNOKII_VERSION_RELEASE @LIBGNOKII_VERSION_RELEASE@
 
 rem Prepare
-echo s/@LIBGNOKII_VERSION_STRING@/%LIBGNOKII_VERSION_STRING%/ > %temp%\gnokii_version.sed
-echo s/@LIBGNOKII_VERSION_MAJOR@/%LIBGNOKII_VERSION_MAJOR%/ >> %temp%\gnokii_version.sed
-echo s/@LIBGNOKII_VERSION_MINOR@/%LIBGNOKII_VERSION_MINOR%/ >> %temp%\gnokii_version.sed
-echo s/@LIBGNOKII_VERSION_RELEASE@/%LIBGNOKII_VERSION_RELEASE%/ >> %temp%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_STRING@/%LIBGNOKII_VERSION_STRING%/ > %TEMP%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_MAJOR@/%LIBGNOKII_VERSION_MAJOR%/ >> %TEMP%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_MINOR@/%LIBGNOKII_VERSION_MINOR%/ >> %TEMP%\gnokii_version.sed
+echo s/@LIBGNOKII_VERSION_RELEASE@/%LIBGNOKII_VERSION_RELEASE%/ >> %TEMP%\gnokii_version.sed
 
 rem Execute sed with parameters
-sed -f %temp%\gnokii_version.sed %GNOKII_H_IN_PATH%\gnokii.h.in > %GNOKII_H_IN_PATH%\gnokii.h
+sed -f %TEMP%\gnokii_version.sed %GNOKII_H_IN_PATH%\gnokii.h.in > %GNOKII_H_IN_PATH%\gnokii.h
 del /q %TEMP%\gnokii_version.sed
 
 echo done!
