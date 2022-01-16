98107a74b8e11b769424282c38007911b5a44399
Pawel Kot <gnokii@gmail.com>
2022-01-16 23:12:57 +0100

    build: use gettext directly
    Get rid of indirect usage of gettext: glib-gettextize and intltool.
    As of version 0.18 gettext makes these obsolete.
    See https://wiki.gnome.org/MigratingFromIntltoolToGettext for
    the reference.

--- autogen.sh
+++ autogen.sh
@@ -5,8 +5,7 @@ if [ "`uname -s`"x = "Darwin"x ]; then
 else
 	libtoolize -c -f
 fi
-glib-gettextize -f -c
-intltoolize --force --copy --automake
+autopoint -f
 AC_LOCAL_FLAGS="-I m4/"
 if [ "`uname -s`"x = "FreeBSD"x ]; then
 	AC_LOCAL_FLAGS="${AC_LOCAL_FLAGS} -I /usr/local/share/aclocal"
