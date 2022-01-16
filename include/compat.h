98949e271449677df00efcd589bad7d8a7aa648f
Pawel Kot <gnokii@gmail.com>
2022-01-16 20:26:12 +0100

    configure.ac: remove reference to obsolete macros
    Remove refrences to obsolete macros AC_HEADER_STDC and AC_HEADER_TIME as per
    https://www.gnu.org/software/autoconf/manual/autoconf-2.70/html_node/Obsolete-Macros.html
    Introduce AC_INCLUDES_DEFAULT to check for C90 standard headers.
    Remove obsolete macros AM_PROG_CC_C_0 and AC_TYPE_SIGNAL.
    Remove checks for TIME_WITH_SYS_TIME.
    Calling AC_PROG_LEX without an argument is obsolete.

--- include/compat.h
+++ include/compat.h
@@ -77,15 +77,9 @@
 #  include <sys/file.h>
 #endif
 
-#ifdef TIME_WITH_SYS_TIME
+#include <time.h>
+#ifdef HAVE_SYS_TIME_H
 #  include <sys/time.h>
-#  include <time.h>
-#else
-#  ifdef HAVE_SYS_TIME_H
-#    include <sys/time.h>
-#  else
-#    include <time.h>
-#  endif
 #endif
 
 #ifdef HAVE_SYS_SOCKET_H
