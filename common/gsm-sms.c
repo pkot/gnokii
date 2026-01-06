pkot/gnokii

commit 6d59b8b1a0f96b41a6160b6ddd1dae2396ff38c5

Move callbacks from gn_data to gn_statemachine->callbacks
Callbacks were registered in gn_data. It might have been dangerous as
various callers might call gn_data_clear() quite often.  It may clear
registered callbacks.  Therefore let's keep it in the statemachine in a
separate new structure.

gnokii/gnokii-sms.c
@@ -1291,7 +1291,7 @@ gn_error smsreader(gn_data *data, struct gn_statemachine *state)
 {
 	gn_error error;
 
-	data->on_sms = smsslave;
+	state->callbacks.on_sms = smsslave;
 	data->callback_data = NULL;
 	error = gn_sm_functions(GN_OP_OnSMS, data, state);
 	if (!error) {
@@ -1308,7 +1308,7 @@ gn_error smsreader(gn_data *data, struct gn_statemachine *state)
 		fprintf(stderr, _("Shutting down\n"));
 
 		fprintf(stderr, _("Exiting sms reader mode...\n"));
-		data->on_sms = NULL;
+		state->callbacks.on_sms = NULL;
 
 		error = gn_sm_functions(GN_OP_OnSMS, data, state);
 		if (error != GN_ERR_NONE)

common/data/at-emulator.c
@@ -142,7 +142,7 @@ bool gn_atem_initialise(int read_fd, int write_fd, struct gn_statemachine *vmsm)
 	MessageFormat = PDU_MODE;
 
 	/* Set the call passup so that we get notified of incoming calls */
-	data.call_notification = gn_atem_call_passup;
+	sm->callbacks.call_notification = gn_atem_call_passup;
 	gn_sm_functions(GN_OP_SetCallNotification, &data, sm);
 
 	/* query model, revision and imei */
@@ -195,7 +195,7 @@ static void  gn_atem_answer_phone(void)
 {
 	/* For now we'll also initialise the datapump + rlp code again */
 	dp_Initialise(PtyRDFD, PtyWRFD);
-	data.call_notification = dp_CallPassup;
+	sm->callbacks.call_notification = dp_CallPassup;
 	gn_sm_functions(GN_OP_SetCallNotification, &data, sm);
 	data.call_info->call_id = IncomingCallNo;
 	gn_sm_functions(GN_OP_AnswerCall, &data, sm);
@@ -372,7 +372,7 @@ void	gn_atem_at_parse(char *cmd_buffer)
 			buf++;
 			if (toupper(*buf) == 'T' || toupper(*buf) == 'P') buf++;
 			while (*buf == ' ') buf++;
-			data.call_notification = dp_CallPassup;
+			sm->callbacks.call_notification = dp_CallPassup;
 			gn_sm_functions(GN_OP_SetCallNotification, &data, sm);
 			snprintf(data.call_info->number, sizeof(data.call_info->number), "%s", buf);
 			if (ModemRegisters[S35] == 0)

common/data/datapump.c
@@ -60,7 +60,7 @@ bool dp_Initialise(int read_fd, int write_fd)
 	rlp_user_request_set(Attach_Req, true);
 	pluscount = 0;
 	connected = false;
-	data.rlp_rx_callback = rlp_f96_frame_display;
+	sm->callbacks.rlp_rx_callback = rlp_f96_frame_display;
 	gn_sm_functions(GN_OP_SetRLPRXCallback, &data, sm);
 
 	return true;
@@ -89,7 +89,7 @@ static int DP_CallBack(rlp_user_inds ind, unsigned char *buffer, int length)
 		if (CommandMode == false) gn_atem_modem_result(MR_NOCARRIER);
 		connected = false;
 		/* Set the call passup back to the at emulator */
-		data.call_notification = gn_atem_call_passup;
+		sm->callbacks.call_notification = gn_atem_call_passup;
 		gn_sm_functions(GN_OP_SetCallNotification, &data, sm);
 		CommandMode = true;
 		break;
@@ -124,7 +124,7 @@ static int DP_CallBack(rlp_user_inds ind, unsigned char *buffer, int length)
 			if (pluscount == 3) {
 				CommandMode = true;
 				/* Set the call passup back to the at emulator */
-				data.call_notification = gn_atem_call_passup;
+				sm->callbacks.call_notification = gn_atem_call_passup;
 				gn_sm_functions(GN_OP_SetCallNotification, &data, sm);
 				gn_atem_string_out("\r\n");
 				gn_atem_modem_result(MR_OK);
@@ -154,7 +154,7 @@ void dp_CallPassup(gn_call_status CallStatus, gn_call_info *CallInfo, struct gn_
 	case GN_CALL_RemoteHangup:
 		CommandMode = true;
 		/* Set the call passup back to the at emulator */
-		data.call_notification = gn_atem_call_passup;
+		sm->callbacks.call_notification = gn_atem_call_passup;
 		gn_sm_functions(GN_OP_SetCallNotification, &data, sm);
 		gn_atem_modem_result(MR_NOCARRIER);
 		rlp_user_request_set(Disc_Req, true);

common/phones/atgen.c
@@ -1589,7 +1589,7 @@ static gn_error AT_SetCallNotification(gn_data *data, struct gn_statemachine *st
 	at_driver_instance *drvinst = AT_DRVINST(state);
 	gn_error err;
 
-	if (!drvinst->call_notification && !data->call_notification)
+	if (!drvinst->call_notification && !state->callbacks.call_notification)
 		return GN_ERR_NONE;
 
 	if (!drvinst->call_notification) {
@@ -1609,7 +1609,7 @@ static gn_error AT_SetCallNotification(gn_data *data, struct gn_statemachine *st
 		sm_block_no_retry(GN_OP_SetCallNotification, data, state);
 	}
 
-	drvinst->call_notification = data->call_notification;
+	drvinst->call_notification = state->callbacks.call_notification;
 	drvinst->call_callback_data = data->callback_data;
 
 	return GN_ERR_NONE;
@@ -1623,7 +1623,7 @@ static gn_error AT_GetNetworkInfo(gn_data *data, struct gn_statemachine *state)
 	 * AT+CREG enables +CREG notifications, so register notification
 	 * callback.
 	 */
-	drvinst->reg_notification = data->reg_notification;
+	drvinst->reg_notification = state->callbacks.reg_notification;
 	drvinst->reg_callback_data = data->callback_data;
 
 	if (!data->network_info)
@@ -1787,7 +1787,7 @@ static gn_error AT_OnSMS(gn_data *data, struct gn_statemachine *state)
 		error = sm_block_no_retry(GN_OP_OnSMS, data, state);
 	} while (mode-- && error);
 	if (error == GN_ERR_NONE) {
-		AT_DRVINST(state)->on_sms = data->on_sms;
+		AT_DRVINST(state)->on_sms = state->callbacks.on_sms;
 		AT_DRVINST(state)->sms_callback_data = data->callback_data;
 	}
 	return error;

common/phones/nk3110.c
@@ -589,7 +589,7 @@ static gn_error P3110_SendRLPFrame(gn_data *data, struct gn_statemachine *state)
 
 static gn_error P3110_SetRLPRXCallback(gn_data *data, struct gn_statemachine *state)
 {
-	DRVINSTANCE(state)->rlp_rx_callback = data->rlp_rx_callback;
+	DRVINSTANCE(state)->rlp_rx_callback = state->callbacks.rlp_rx_callback;
 
 	return GN_ERR_NONE;
 }

common/phones/nk6100.c
@@ -1310,8 +1310,8 @@ static gn_error SetCellBroadcast(gn_data *data, struct gn_statemachine *state)
 	if (DRVINSTANCE(state)->capabilities & NK6100_CAP_NO_CB)
 		return GN_ERR_NOTSUPPORTED;
 
-	req = data->on_cell_broadcast ? req_ena : req_dis;
-	DRVINSTANCE(state)->on_cell_broadcast = data->on_cell_broadcast;
+	req = state->callbacks.on_cell_broadcast ? req_ena : req_dis;
+	DRVINSTANCE(state)->on_cell_broadcast = state->callbacks.on_cell_broadcast;
 	DRVINSTANCE(state)->cb_callback_data = data->callback_data;
 
 	if (sm_message_send(10, 0x02, req, state)) return GN_ERR_NOTREADY;
@@ -1407,8 +1407,8 @@ static void FlushLostSMSNotifications(struct gn_statemachine *state)
 
 static gn_error SetOnSMS(gn_data *data, struct gn_statemachine *state)
 {
-	if (data->on_sms) {
-		DRVINSTANCE(state)->on_sms = data->on_sms;
+	if (state->callbacks.on_sms) {
+		DRVINSTANCE(state)->on_sms = state->callbacks.on_sms;
 		DRVINSTANCE(state)->sms_callback_data = data->callback_data;
 		DRVINSTANCE(state)->sms_notification_lost = true;
 		FlushLostSMSNotifications(state);
@@ -3364,7 +3364,7 @@ static gn_error CancelCall1(gn_data *data, struct gn_statemachine *state)
 
 static gn_error SetCallNotification(gn_data *data, struct gn_statemachine *state)
 {
-	DRVINSTANCE(state)->call_notification = data->call_notification;
+	DRVINSTANCE(state)->call_notification = state->callbacks.call_notification;
 	DRVINSTANCE(state)->call_callback_data = data->callback_data;
 
 	return GN_ERR_NONE;
@@ -3599,7 +3599,7 @@ static gn_error SendRLPFrame(gn_data *data, struct gn_statemachine *state)
 
 static gn_error SetRLPRXCallback(gn_data *data, struct gn_statemachine *state)
 {
-	DRVINSTANCE(state)->rlp_rx_callback = data->rlp_rx_callback;
+	DRVINSTANCE(state)->rlp_rx_callback = state->callbacks.rlp_rx_callback;
 
 	return GN_ERR_NONE;
 }

common/phones/nk6510.c
@@ -426,11 +426,11 @@ static gn_error NK6510_Functions(gn_operation op, gn_data *data, struct gn_state
 	case GN_OP_WriteWAPSetting:
 		return NK6510_WriteWAPSetting(data, state);
 	case GN_OP_OnSMS:
-		DRVINSTANCE(state)->on_sms = data->on_sms;
+		DRVINSTANCE(state)->on_sms = state->callbacks.on_sms;
 		DRVINSTANCE(state)->sms_callback_data = data->callback_data;
 		return NK6510_Subscribe(data, state);
 	case GN_OP_SetCallNotification:
-		DRVINSTANCE(state)->call_notification = data->call_notification;
+		DRVINSTANCE(state)->call_notification = state->callbacks.call_notification;
 		DRVINSTANCE(state)->call_callback_data = data->callback_data;
 		return NK6510_Subscribe(data, state);
 	case GN_OP_DeleteSMS:
@@ -2282,8 +2282,8 @@ static gn_error NK6510_GetFile(gn_data *data, struct gn_statemachine *state)
 	int i;
 
 	/* Register callback */
-	if (data->progress_indication) {
-		DRVINSTANCE(state)->progress_indication = data->progress_indication;
+	if (state->callbacks.progress_indication) {
+		DRVINSTANCE(state)->progress_indication = state->callbacks.progress_indication;
 		DRVINSTANCE(state)->progress_callback_data = data->callback_data;
 	}
 
@@ -2363,8 +2363,8 @@ static gn_error NK6510_GetFileById(gn_data *data, struct gn_statemachine *state)
 	int i, length;
 
 	/* Register callback */
-	if (data->progress_indication) {
-		DRVINSTANCE(state)->progress_indication = data->progress_indication;
+	if (state->callbacks.progress_indication) {
+		DRVINSTANCE(state)->progress_indication = state->callbacks.progress_indication;
 		DRVINSTANCE(state)->progress_callback_data = data->callback_data;
 	}
 

common/phones/nk7110.c
@@ -235,10 +235,10 @@ static gn_error NK7110_Functions(gn_operation op, gn_data *data, struct gn_state
 		dprintf("Getting SMS (no validating)...\n");
 		return NK7110_GetSMSnoValidate(data, state);
 	case GN_OP_OnSMS:
-		DRVINSTANCE(state)->on_sms = data->on_sms;
+		DRVINSTANCE(state)->on_sms = state->callbacks.on_sms;
 		DRVINSTANCE(state)->sms_callback_data = data->callback_data;
 		/* Register notify when running for the first time */
-		if (data->on_sms) {
+		if (state->callbacks.on_sms) {
 			DRVINSTANCE(state)->new_sms = true;
 			return GN_ERR_NONE; /* FIXME NK7110_GetIncomingSMS(data, state); */
 		}

commit 84c6177210855ebce89cf8b40981410d65004ef1

Move gn_sms_delivery_report_status into union
This API breaking change should be done a long time ago. Do it
now as we already broke API with commit 6d59b8b1a0f9
("Move callbacks from gn_data to gn_statemachine->callbacks")

common/gsm-sms.c
@@ -317,11 +317,10 @@ static unsigned int validity2minutes(unsigned char value)
  */
 static gn_error sms_status(unsigned char status, gn_sms *sms)
 {
-	sms->user_data[0].type = GN_SMS_DATA_Text;
+	sms->user_data[0].type = GN_SMS_DATA_DRStatus;
 	sms->user_data[1].type = GN_SMS_DATA_None;
 	if (status < 0x03) {
-		sms->user_data[0].dr_status = GN_SMS_DR_Status_Delivered;
-		snprintf(sms->user_data[0].u.text, sizeof(sms->user_data[0].u.text), "%s", _("Delivered"));
+		sms->user_data[0].u.dr_status = GN_SMS_DR_Status_Delivered;
 		switch (status) {
 		case 0x00:
 			dprintf("SM received by the SME");
@@ -334,11 +333,9 @@ static gn_error sms_status(unsigned char status, gn_sms *sms)
 			break;
 		}
 	} else if (status & 0x40) {
-		snprintf(sms->user_data[0].u.text, sizeof(sms->user_data[0].u.text), "%s", _("Failed"));
-		/* more detailed reason only for debug */
 		if (status & 0x20) {
+			sms->user_data[0].u.dr_status = GN_SMS_DR_Status_Failed_Permanent;
 			dprintf("Temporary error, SC is not making any more transfer attempts\n");
-			sms->user_data[0].dr_status = GN_SMS_DR_Status_Failed_Permanent;
 			switch (status) {
 			case 0x60:
 				dprintf("Congestion");
@@ -363,8 +360,8 @@ static gn_error sms_status(unsigned char status, gn_sms *sms)
 				break;
 			}
 		} else {
+			sms->user_data[0].u.dr_status = GN_SMS_DR_Status_Failed_Temporary;
 			dprintf("Permanent error, SC is not making any more transfer attempts\n");
-			sms->user_data[0].dr_status = GN_SMS_DR_Status_Failed_Temporary;
 			switch (status) {
 			case 0x40:
 				dprintf("Remote procedure error");
@@ -402,9 +399,7 @@ static gn_error sms_status(unsigned char status, gn_sms *sms)
 			}
 		}
 	} else if (status & 0x20) {
-		sms->user_data[0].dr_status = GN_SMS_DR_Status_Pending;
-		snprintf(sms->user_data[0].u.text, sizeof(sms->user_data[0].u.text), "%s", _("Pending"));
-		/* more detailed reason only for debug */
+		sms->user_data[0].u.dr_status = GN_SMS_DR_Status_Pending;
 		dprintf("Temporary error, SC still trying to transfer SM\n");
 		switch (status) {
 		case 0x20:
@@ -430,14 +425,10 @@ static gn_error sms_status(unsigned char status, gn_sms *sms)
 			break;
 		}
 	} else {
-		sms->user_data[0].dr_status = GN_SMS_DR_Status_Invalid;
-		snprintf(sms->user_data[0].u.text, sizeof(sms->user_data[0].u.text), "%s", _("Unknown"));
-
-		/* more detailed reason only for debug */
+		sms->user_data[0].u.dr_status = GN_SMS_DR_Status_Invalid;
 		dprintf("Reserved/Specific to SC: 0x%02x", status);
 	}
 	dprintf("\n");
-	sms->user_data[0].length = strlen(sms->user_data[0].u.text);
 	return GN_ERR_NONE;
 }
 
@@ -755,7 +746,6 @@ static gn_error sms_pdu_decode(gn_sms_raw *rawsms, gn_sms *sms)
 
 	error = sms_header_decode(rawsms, sms, &sms->udh);
 	ERROR();
-	sms->user_data[0].dr_status = GN_SMS_DR_Status_None;
 	switch (sms->type) {
 	case GN_SMS_MT_DeliveryReport:
 	case GN_SMS_MT_StatusReport:

gnokii/gnokii-sms.c
@@ -761,7 +761,29 @@ parsefile:
 				fprintf(stdout, "\n");
 				fprintf(stdout, _("Recipient: %s Msg Center: %s\n"), message.remote.number, message.smsc.number);
 				fprintf(stdout, _("Text:\n"));
-				message_text = message.user_data[0].u.text;
+				if (message.user_data[0].type == GN_SMS_DATA_DRStatus) {
+					switch (message.user_data[0].u.dr_status) {
+					case GN_SMS_DR_Status_None:
+						fprintf(stdout, _("None"));
+						break;
+					case GN_SMS_DR_Status_Invalid:
+						fprintf(stdout, _("Unknown"));
+						break;
+					case GN_SMS_DR_Status_Delivered:
+						fprintf(stdout, _("Delivered"));
+						break;
+					case GN_SMS_DR_Status_Pending:
+						fprintf(stdout, _("Pending"));
+						break;
+					case GN_SMS_DR_Status_Failed_Temporary:
+					case GN_SMS_DR_Status_Failed_Permanent:
+						fprintf(stdout, _("Failed"));
+						break;
+					}
+					fprintf(stdout, "\n");
+				} else {
+					dprintf("Unexpected data type\n");
+				}
 				break;
 			case GN_SMS_MT_Picture:
 			case GN_SMS_MT_PictureTemplate:

include/gnokii/sms.h
@@ -324,7 +324,8 @@ typedef enum {
 	GN_SMS_DATA_Animation = 0x07,
 	GN_SMS_DATA_Concat    = 0x08,
 	GN_SMS_DATA_WAPPush   = 0x09,
-	GN_SMS_DATA_Other     = 0x0a
+	GN_SMS_DATA_Other     = 0x0a,
+	GN_SMS_DATA_DRStatus  = 0x0b
 } gn_sms_data_type;
 
 /*** FOLDER INFO ***/
@@ -372,12 +373,8 @@ typedef struct {
 		gn_ringtone ringtone;
 		gn_bmp animation[4];
 		gn_sms_concat concat;
+		gn_sms_delivery_report_status dr_status;
 	} u;
-	/* That should be in the union, but for delivery reports we already
-	 * set text there. Currently we don't want to break API, so I put it here
-	 * Pawel Kot, 2007-11-21
-	 */
-	gn_sms_delivery_report_status dr_status;
 } gn_sms_user_data;
 
 /* Define datatype for SMS messages exported to the user applications. */

smsd/smsd.c
@@ -431,6 +431,27 @@ static void ReadSMS (gpointer d, gpointer userData)
 
   if (data->type == GN_SMS_MT_Deliver || data->type == GN_SMS_MT_StatusReport)
   {
+    /* Plugins expect delivery status as a text message */
+    if (data->user_data[0].type == GN_SMS_DATA_DRStatus) {
+      switch (data->user_data[0].u.dr_status) {
+      case GN_SMS_DR_Status_None:
+        strncpy(data->user_data[0].u.text, _("None"), sizeof(data->user_data[0].u.text));
+        break;
+      case GN_SMS_DR_Status_Invalid:
+        strncpy(data->user_data[0].u.text, _("Unknown"), sizeof(data->user_data[0].u.text));
+	break;
+      case GN_SMS_DR_Status_Delivered:
+        strncpy(data->user_data[0].u.text, _("Delivered"), sizeof(data->user_data[0].u.text));
+	break;
+      case GN_SMS_DR_Status_Pending:
+        strncpy(data->user_data[0].u.text, _("Pending"), sizeof(data->user_data[0].u.text));
+	break;
+      case GN_SMS_DR_Status_Failed_Temporary:
+      case GN_SMS_DR_Status_Failed_Permanent:
+        strncpy(data->user_data[0].u.text, _("Failed"), sizeof(data->user_data[0].u.text));
+        break;
+      }
+    }
     gn_log_xdebug ("%d. %s   ", data->number, data->remote.number);
     gn_log_xdebug ("%02d-%02d-%02d %02d:%02d:%02d+%02d %s\n", data->smsc_time.year,
                     data->smsc_time.month, data->smsc_time.day, data->smsc_time.hour,

xgnokii/xgnokii_sms.c
@@ -340,10 +340,34 @@ static void InsertFolderElement(gpointer d, gpointer userData)
 		row[2] = GUI_GetName(data->remote.number);
 		if (row[2] == NULL)
 			row[2] = data->remote.number;
-		if ((data->type == GN_SMS_MT_Picture) || (data->type == GN_SMS_MT_PictureTemplate))
+
+		if ((data->type == GN_SMS_MT_Picture) || (data->type == GN_SMS_MT_PictureTemplate)) {
 			row[3] = g_strdup_printf(_("Picture Message: %s"), data->user_data[1].u.text);
-		else
-			row[3] = data->user_data[0].u.text;
+		} else if (data->user_data[0].type == GN_SMS_DATA_DRStatus) {
+			switch (data->user_data[0].u.dr_status) {
+			case GN_SMS_DR_Status_None:
+				row[3] = g_strdup(_("None"));
+				break;
+			case GN_SMS_DR_Status_Invalid:
+				row[3] = g_strdup(_("Unknown"));
+				break;
+			case GN_SMS_DR_Status_Delivered:
+				row[3] = g_strdup(_("Delivered"));
+				break;
+			case GN_SMS_DR_Status_Pending:
+				row[3] = g_strdup(_("Pending"));
+				break;
+			case GN_SMS_DR_Status_Failed_Temporary:
+			case GN_SMS_DR_Status_Failed_Permanent:
+				row[3] = g_strdup(_("Failed"));
+				break;
+			default:
+				row[3] = g_strdup(_("Unexpected"));
+				break;
+			}
+		} else {
+			row[3] = g_strdup(data->user_data[0].u.text);
+		}
 
 		gtk_clist_append(GTK_CLIST(SMS.smsClist), row);
 
@@ -360,6 +384,7 @@ static void InsertFolderElement(gpointer d, gpointer userData)
 					    msgPtrs, DestroyMsgPtrs);
 		g_free(row[0]);
 		g_free(row[1]);
+		g_free(row[3]);
 	}
 }
 