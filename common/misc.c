80b5054bb0eddf72b00f9dca8515724ce552ac67
Ladislav Michl <ladis@triops.cz>
2026-07-27 18:26:54 +0200

    Fix C2-03 phone ID
    Fixes: af8beaae92c ("Add phone ID for C2-03 (series40 3rd)")

--- common/misc.c
+++ common/misc.c
@@ -320,7 +320,7 @@ static gn_phone_model models[] = {
 	{"RPM-1", "RPM-1",	PM_OLD_DEFAULT | PM_DATA },
 	{"C1-01", "RM-607",	PM_DEFAULT_S40_3RD },  /* Series 40 6th Edition Lite */
 	{"C1-02", "RM-643",	PM_DEFAULT_S40_3RD },  /* Series 40 6th Edition Lite */
-	{"C2-03", "RM-70",	PM_DEFAULT_S40_3RD },
+	{"C2-03", "RM-702",	PM_DEFAULT_S40_3RD },  /* Series 40 6th Edition feature pack 1 */
 	{"Card Phone 1.0", "RPE-1",	PM_OLD_DEFAULT | PM_DATA },
 	{"Card Phone 2.0", "RPM-1",	PM_OLD_DEFAULT | PM_DATA },
 	{"C110 Wireless LAN Card", "DTN-10",	PM_OLD_DEFAULT },
